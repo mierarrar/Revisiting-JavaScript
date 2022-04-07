3d173c40259db08b6a0440564133a9b9592b41fc
MeeraNair07 <meera07nair@gmail.com>
2022-04-07 16:09:26 +0300

    type conversion

---
+++ JS: a do-over/typeConversion.js
@@ -0,0 +1,20 @@
+// String Conversion
+
+let value = true;
+console.log(value);
+value = String(value);
+console.log(typeof value);
+
+// Number Conversion
+
+let str = "123";
+num = Number(str);
+console.log(num + 5);
+console.log(typeof num);
+
+// Boolean Conversion
+
+// Values that are intuitively “empty”, like 0, an empty string, null, undefined, and NaN, become false.
+// Other values become true.
+
+console.log( Boolean("") ); // false
