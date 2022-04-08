25c50d21f58fe0e7141eb123f7d6efb15749805e
MeeraNair07 <meera07nair@gmail.com>
2022-04-08 22:29:31 +0300

    logical operators

--- JS: a do-over/logicalOperators.js
+++ JS: a do-over/logicalOperators.js
@@ -1,2 +1,19 @@
-result = Boolean(0) && !!0;
-console.log(result)
+// result = Boolean(0) && !!0;
+// console.log(result)
+
+let userValue = prompt("Who's there?", "");
+
+if (userValue == " " || userValue == null){
+    alert("Cancelled")
+} else if (userValue == "Admin"){
+    let password = prompt("Password?")
+    if (password == " " || password == null){
+        alert("Cancelled");
+    } else if (password == "TheMaster"){
+        alert("Welcome!")
+    } else {
+        alert("I don't know you")
+    }
+} else {
+    alert("I don't know you")
+}
