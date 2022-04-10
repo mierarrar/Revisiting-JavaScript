44836ffd3a910d6691b3e99c2707f3275a4cbeeb
MeeraNair07 <meera07nair@gmail.com>
2022-04-10 12:08:49 +0300

    functions

---
+++ JS: a do-over/Javascript Fundamentals/functions.js
@@ -0,0 +1,20 @@
+function min(a, b) {
+    return a < b ? a : b;
+  }
+
+function pow(x,n) {
+    let result = x;
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+    return result;
+}
+
+let x = prompt ("x?", '');
+let n = prompt ("n?", '');
+
+if (n < 1){
+    alert("NO");
+} else {
+    alert(pow(x,n))
+}
