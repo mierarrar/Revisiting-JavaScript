2e050df79964a0cb794597edc813ebf37780fe06
MeeraNair07 <meera07nair@gmail.com>
2022-09-18 20:59:17 +0300

    <3

---
+++ JS: a do-over/Brower : Documents, Events, Interfaces /A Simple Form App/main.js
@@ -0,0 +1,46 @@
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item));
+
+// const ul = document.querySelector('.items');
+// ul.remove();
+// ul.lastElementChild.remove();
+
+// ul.firstElementChild.textContent = 'Hello!';
+// ul.children[1].innerText = "Plumbus";
+// ul.lastElementChild.innerHTML = "<h1>HELLO</h1>";
+
+// const btn = document.querySelector('.btn');
+// btn.style.background = "red";
+
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     // document.querySelector('body').classList.add('bg-dark');
+//     // document.querySelector('body').classList.remove('bg-dark');
+// });
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    // console.log(nameInput.value);
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields'; 
+        
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userList.appendChild(li);
+
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
